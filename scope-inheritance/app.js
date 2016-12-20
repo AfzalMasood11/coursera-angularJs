ad377097dde11e818a9734a98ed2972a2584d657
Afzal Masood <afzal.masood11@gmail.com>
2016-12-20 16:40:14 +0500

    practice of scope inheritance as syntax.

--- scope-inheritance/app.js
+++ scope-inheritance/app.js
@@ -17,33 +17,33 @@ function ParentController1($scope) {
 
 ChildController1.$inject = ['$scope'];
 function ChildController1($scope){
-  console.log('$scope.parentValue: ', $scope.parentValue);
-  console.log('CHILD  $scope: ', $scope);
-  $scope.parentValue = 5;
-  console.log(' *** CHANGED: $scope.parentValue = 5 ***');
-  console.log('$scope.parentValue: ', $scope.parentValue);
-  console.log($scope);
+  // console.log('$scope.parentValue: ', $scope.parentValue);
+  // console.log('CHILD  $scope: ', $scope);
+  // $scope.parentValue = 5;
+  // console.log(' *** CHANGED: $scope.parentValue = 5 ***');
+  // console.log('$scope.parentValue: ', $scope.parentValue);
+  // console.log($scope);
 
-  console.log('$scope.pc.parentValue: ', $scope.pc.parentValue);
-  $scope.pc.parentValue = 5;
-  console.log(' *** CHANGED: $scope.pc.parentValue = 5 ***');
-  console.log('$scope.pc.parentValue: ', $scope.pc.parentValue);
-  console.log('$SCOPE',$scope);
+  // console.log('$scope.pc.parentValue: ', $scope.pc.parentValue);
+  // $scope.pc.parentValue = 5;
+  // console.log(' *** CHANGED: $scope.pc.parentValue = 5 ***');
+  // console.log('$scope.pc.parentValue: ', $scope.pc.parentValue);
+  // console.log('$SCOPE',$scope);
 
-  console.log('$scope.$parent.parentValue', $scope.$parent.parentValue);
+  // console.log('$scope.$parent.parentValue', $scope.$parent.parentValue);
 }
 
-ParentController2.$inject = ['$scope'];
-function ParentController2($scope) {
-  // $scope.parentValue = 1;
-  // $scope.pc = this;
-  // $scope.pc.parentValue = 1;
+// Controller as Syntax 
+function ParentController2() {
+  var parent = this;
+  parent.value = 1;
 }
 
 ChildController2.$inject = ['$scope'];
 function ChildController2($scope){
-  // console.log('$scope.parentValue: ', $scope.parentValue);
-  // console.log('CHILD  $scope: ', $scope);
+  var child = this;
+  child.value = 5;
+  console.log('ChildController2 $scope: ', $scope);
 }
 
 
