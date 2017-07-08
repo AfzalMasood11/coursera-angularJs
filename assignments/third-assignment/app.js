9e1abac97e4c93deb4f14d06b6983ca80959d0e9
Afzal Masood <afzal.masood11@gmail.com>
2017-07-08 14:12:32 +0500

    fix https issue.

--- assignments/third-assignment/app.js
+++ assignments/third-assignment/app.js
@@ -4,7 +4,7 @@
   .controller('NarrowItDownController', NarrowItDownController)
   .service('MenuSearchService', MenuSearchService)
   .directive('foundItems', FoundItemsDirective)
-  .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+  .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
   function FoundItemsDirective() {
     var ddo = {
